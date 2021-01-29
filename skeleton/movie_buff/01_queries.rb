4fbff47d81fae4216b2febfa8b68d2e28bf8b169
Lakhte <lakhteagha@gmail.com>
2021-01-29 16:28:53 -0500

    finished part 2

--- skeleton/movie_buff/01_queries.rb
+++ skeleton/movie_buff/01_queries.rb
@@ -22,7 +22,10 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-  Actor.joins(:castings, :movies).select(movies.id, movies.title).where.not(castings: {ord: 1}).where(actors: {name: 'Harrison Ford'})
+  Movie.joins(:actors)
+        .select(:id, :title)
+        .where.not(castings: {ord: 1})
+        .where(actors: {name: 'Harrison Ford'})
 end
 
 def biggest_cast
@@ -39,7 +42,10 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
-
+  Movie.select(:id, :title).joins(:actors)
+        .group(:id)
+        .order('COUNT(castings.actor_id) DESC')
+        .limit(3)
 end
 
 def directed_by_one_of(them)
@@ -54,7 +60,7 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-
+  Movie.select(:id, :title).joins(:director).where(actors: { name: them })
 end
 
 def movie_names_before_1940
@@ -68,5 +74,5 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie.where('yr < 1940').pluck(:title)
 end
